adff7a7b517954b627290919c8a19aa7c3d3ef3c
phitchcock <phitchcock@me.com>
2014-10-01 10:37:07 -0700

    created helper function loadUrl to grab url from textfield

--- MySafari/ViewController.swift
+++ MySafari/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController, UIWebViewDelegate {
+class ViewController: UIViewController, UIWebViewDelegate, UITextFieldDelegate {
 
     //IBOutlets
     @IBOutlet weak var webView: UIWebView!
@@ -18,11 +18,27 @@ class ViewController: UIViewController, UIWebViewDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        loadUrl("http://www.google.com")
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+
+        let urlString = urlTextField.text
+
+        return true
+
+    }
+
+    func loadUrl(urlString: NSString) {
+
+        let url = NSURL(string: urlString)
+        let urlRequest = NSURLRequest(URL: url)
+        self.webView.loadRequest(urlRequest)
     }
 
 
