caa977e4aefa505a87870208a4fff55cadc3637b
phitchcock <phitchcock@me.com>
2014-10-01 10:42:50 -0700

    implemented load and finished networkActivityIndicator methods

--- MySafari/ViewController.swift
+++ MySafari/ViewController.swift
@@ -14,24 +14,33 @@ class ViewController: UIViewController, UIWebViewDelegate, UITextFieldDelegate {
     @IBOutlet weak var webView: UIWebView!
     @IBOutlet weak var urlTextField: UITextField!
 
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
         loadUrl("http://www.google.com")
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+
     }
 
     func textFieldShouldReturn(textField: UITextField) -> Bool {
 
         let urlString = urlTextField.text
+        loadUrl(urlString)
 
         return true
+    }
 
+    func webViewDidStartLoad(webView: UIWebView) {
+
+        UIApplication.sharedApplication().networkActivityIndicatorVisible = true
+    }
+
+    func webViewDidFinishLoad(webView: UIWebView) {
+
+        UIApplication.sharedApplication().networkActivityIndicatorVisible = false
     }
 
     func loadUrl(urlString: NSString) {
